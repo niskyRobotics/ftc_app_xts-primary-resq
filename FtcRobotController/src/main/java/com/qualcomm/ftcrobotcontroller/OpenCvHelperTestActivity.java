ae40e7e0fdb4fa09f2cb8091318ea089a61b0e01
hexafraction <hexafraction@gmail.com>
2015-09-14 21:04:38 -0400

    working opencv preview, but will need to be refactored later

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/OpenCvHelperTestActivity.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/OpenCvHelperTestActivity.java
@@ -1,39 +1,72 @@
 package com.qualcomm.ftcrobotcontroller;
 
 import android.app.Activity;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.hardware.Camera;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import ftc.team6460.javadeck.ftc.vision.OpenCvImageCaptureHelper;
+import ftc.team6460.javadeck.ftc.vision.OpenCvActivity;
+import org.bytedeco.javacpp.indexer.FloatIndexer;
+import org.bytedeco.javacpp.opencv_core;
+
+import static org.bytedeco.javacpp.opencv_core.CV_32FC1;
+import static org.bytedeco.javacpp.opencv_core.CV_8UC1;
+import static org.bytedeco.javacpp.opencv_imgproc.*;
 
 public class OpenCvHelperTestActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(org.ftc.opmodes.R.layout.activity_open_cv_helper_test);
-
-    }
-
-    @Override
-    protected void onStart() {
-        Log.e("A", "CLICKED");
-        new OpenCvImageCaptureHelper(OpenCvHelperTestActivity.this, null, new Camera.PreviewCallback() {
+        setContentView(R.layout.activity_open_cv_helper_test);
+        findViewById(R.id.btnStart).setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onPreviewFrame(byte[] bytes, Camera camera) {
-                Log.i("F", "frame!");
+            public synchronized void onClick(View view) {
+                Log.e("A", "CLICKED");
+                OpenCvActivity.addCallback(new OpenCvActivity.MatCallback() {
+
+                    opencv_core.Mat circles = new opencv_core.Mat(100, 3, CV_32FC1);
+                    opencv_core.Mat gray = new opencv_core.Mat();
+                    @Override
+                    public void handleMat(opencv_core.Mat mat) {
+                        Log.e("MAT", "PROCESSING");
+                        if (gray == null || gray.arrayWidth() != mat.arrayWidth() || gray.arrayHeight() != mat.arrayHeight()) {
+                            Log.i("PREPROC", "Remaking yuv");
+                            gray.create(mat.arrayHeight(), mat.arrayWidth(), CV_8UC1);
+                        }
+                        cvtColor(mat, gray, COLOR_RGB2GRAY);
+                        HoughCircles(gray, circles, CV_HOUGH_GRADIENT, 1, gray.rows()/8, 200, 100, 0, 0 );
+                    }
+
+                    @Override
+                    public synchronized void draw(Canvas canvas) {
+                        Paint p = new Paint();
+                        p.setColor(Color.GREEN);
+                        FloatIndexer cIdx = circles.createIndexer();
+                        for(int i = 0; i < circles.rows(); i++){
+                            float x = cIdx.get(i,0);
+                            float y = cIdx.get(i,1);
+                            float r = cIdx.get(i,2);
+                            canvas.drawCircle(x,y,r, p);
+                        }
+                    }
+                });
+                OpenCvActivity.startActivity(OpenCvHelperTestActivity.this);
             }
-        }).start();
-        super.onStart();
+        });
+
     }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(org.ftc.opmodes.R.menu.menu_open_cv_helper_test, menu);
+        getMenuInflater().inflate(R.menu.menu_open_cv_helper_test, menu);
         return true;
     }
 
@@ -49,7 +82,7 @@ public class OpenCvHelperTestActivity extends Activity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == org.ftc.opmodes.R.id.action_settings) {
+        if (id == R.id.action_settings) {
             return true;
         }
 
