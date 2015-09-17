89a3a5311657cf35ee5c44317536a1256648f0bf
hexafraction <hexafraction@gmail.com>
2015-09-16 20:50:00 -0400

    tunable circles

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/OpenCvHelperTestActivity.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/OpenCvHelperTestActivity.java
@@ -1,15 +1,18 @@
 package com.qualcomm.ftcrobotcontroller;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.hardware.Camera;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.EditText;
 import ftc.team6460.javadeck.ftc.vision.OpenCvActivity;
 import org.bytedeco.javacpp.indexer.FloatIndexer;
 import org.bytedeco.javacpp.opencv_core;
@@ -19,11 +22,30 @@ import static org.bytedeco.javacpp.opencv_core.*;
 import static org.bytedeco.javacpp.opencv_imgproc.*;
 
 public class OpenCvHelperTestActivity extends Activity {
+    protected double getCanny() {
+        return Double.parseDouble(((EditText) findViewById(R.id.valCanny)).getText().toString());
+    }
+
+    protected double getCentre() {
+        return Double.parseDouble(((EditText) findViewById(R.id.valCanny)).getText().toString());
+    }
+
+    protected int getDiv() {
+        return Integer.parseInt(((EditText) findViewById(R.id.divFactor)).getText().toString());
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_open_cv_helper_test);
+
+        findViewById(R.id.filePickBtn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                pickFileAndStartViewer();
+            }
+        });
+
         findViewById(R.id.btnStart).setOnClickListener(new View.OnClickListener() {
             @Override
             public synchronized void onClick(View view) {
@@ -32,7 +54,11 @@ public class OpenCvHelperTestActivity extends Activity {
                     opencv_core.CvMemStorage str = opencv_core.cvCreateMemStorage();
                     opencv_core.Mat gray = new opencv_core.Mat();
                     opencv_core.Mat grayHalf = new opencv_core.Mat();
+                    double canny = getCanny();
+                    double ctr = getCentre();
+                    int div = getDiv();
                     opencv_core.CvSeq circles;
+
                     @Override
                     public synchronized void handleMat(opencv_core.Mat mat) {
                         Log.e("MAT", "PROCESSING");
@@ -40,37 +66,65 @@ public class OpenCvHelperTestActivity extends Activity {
                             Log.i("PREPROC", "Remaking gray");
                             gray.create(mat.arrayHeight(), mat.arrayWidth(), CV_8UC1);
                         }
-                        if (grayHalf == null || grayHalf.arrayWidth() != mat.arrayWidth()/2 || grayHalf.arrayHeight() != mat.arrayHeight()/2) {
+                        if (grayHalf == null || grayHalf.arrayWidth() != mat.arrayWidth() / div || grayHalf.arrayHeight() != mat.arrayHeight() / div) {
                             Log.i("PREPROC", "Remaking grayhalf");
-                            grayHalf.create(mat.arrayHeight()/2, mat.arrayWidth()/2, CV_8UC1);
+                            grayHalf.create(mat.arrayHeight() / div, mat.arrayWidth() / div, CV_8UC1);
                         }
                         cvtColor(mat, gray, COLOR_RGB2GRAY);
                         cvResize(gray.asIplImage(), grayHalf.asIplImage());
-                        cvSmooth(grayHalf.asIplImage(),grayHalf.asIplImage());
+                        cvSmooth(grayHalf.asIplImage(), grayHalf.asIplImage());
 
-                        circles = cvHoughCircles(grayHalf.asIplImage(), str, opencv_imgproc.CV_HOUGH_GRADIENT,1,grayHalf.rows()/8, 200, 100, 20,400);
+                        circles = cvHoughCircles(grayHalf.asIplImage(), str, opencv_imgproc.CV_HOUGH_GRADIENT, 1, grayHalf.rows() / 8, canny, ctr, 20, 400);
                     }
 
                     @Override
                     public synchronized void draw(Canvas canvas) {
                         if (circles == null) return;
                         Paint p = new Paint();
+                        p.setStrokeWidth(4);
                         p.setStyle(Paint.Style.STROKE);
                         p.setColor(Color.GREEN);
                         Log.i("CIRCLE", new CvPoint3D32f(cvGetSeqElem(circles, 0)).toString());
 
-                        for (int i = 0; (i < circles.total())&&(i<20); i++) {
+                        for (int i = 0; (i < circles.total()) && (i < 20); i++) {
                             opencv_core.CvPoint3D32f pt = new CvPoint3D32f(cvGetSeqElem(circles, i));
-                            canvas.drawCircle(pt.x()*2, pt.y()*2, pt.z()*2, p);
+                            canvas.drawCircle(pt.x() * div, pt.y() * div, pt.z() * div, p);
                         }
                     }
                 });
+
+
                 OpenCvActivity.startActivity(OpenCvHelperTestActivity.this);
             }
         });
 
     }
 
+    private static final int BROWSE_IMAGE_REQUEST_CODE = -74414833;
+
+    private void pickFileAndStartViewer() {
+        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+        intent.setType("image/*");
+        startActivityForResult(intent, BROWSE_IMAGE_REQUEST_CODE);
+
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (RESULT_OK == resultCode &&
+                BROWSE_IMAGE_REQUEST_CODE == requestCode) {
+            Uri uri = data.getData();
+            Intent intent = new Intent(this, ViewProcessedImageActivity.class);
+            intent.putExtra("file_uri", uri);
+            intent.putExtra("div", getDiv());
+            intent.putExtra("canny", getCanny());
+            intent.putExtra("centre", getCentre());
+
+            this.startActivity(intent);
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
---
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/ViewProcessedImageActivity.java
@@ -0,0 +1,153 @@
+package com.qualcomm.ftcrobotcontroller;
+
+import android.app.Activity;
+import android.content.Context;
+import android.database.Cursor;
+import android.graphics.*;
+import android.graphics.Rect;
+import android.hardware.Camera;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.ParcelFileDescriptor;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.*;
+import android.widget.FrameLayout;
+import ftc.team6460.javadeck.ftc.vision.OpenCvActivity;
+import org.bytedeco.javacpp.opencv_core;
+import org.bytedeco.javacpp.opencv_features2d;
+import org.bytedeco.javacpp.opencv_highgui;
+import org.bytedeco.javacpp.opencv_imgproc;
+
+import java.io.FileDescriptor;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicReference;
+
+import static org.bytedeco.javacpp.opencv_core.*;
+import static org.bytedeco.javacpp.opencv_imgproc.*;
+import static org.bytedeco.javacpp.opencv_imgproc.cvHoughCircles;
+
+
+public class ViewProcessedImageActivity extends Activity {
+
+    private FrameLayout layout;
+    private Preview mPreview;
+    private int div;
+    private OpenCvActivity.MatCallback mc;
+
+    Bitmap bmp;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        layout = new FrameLayout(this);
+        mPreview = new Preview(this);
+        Uri uri = getIntent().getParcelableExtra("file_uri");
+        mc = new OpenCvActivity.MatCallback() {
+            opencv_core.CvMemStorage str = opencv_core.cvCreateMemStorage();
+            opencv_core.Mat gray = new opencv_core.Mat();
+            opencv_core.Mat grayHalf = new opencv_core.Mat();
+            double canny = getIntent().getDoubleExtra("canny", 200.0);
+            double ctr = getIntent().getDoubleExtra("centre", 100.0);
+            int div = getIntent().getIntExtra("div", 2);
+            opencv_core.CvSeq circles;
+
+            @Override
+            public synchronized void handleMat(opencv_core.Mat mat) {
+                Log.e("MAT", "PROCESSING");
+                if (gray == null || gray.arrayWidth() != mat.arrayWidth() || gray.arrayHeight() != mat.arrayHeight()) {
+                    Log.i("PREPROC", "Remaking gray");
+                    gray.create(mat.arrayHeight(), mat.arrayWidth(), CV_8UC1);
+                }
+                if (grayHalf == null || grayHalf.arrayWidth() != mat.arrayWidth() / div || grayHalf.arrayHeight() != mat.arrayHeight() / div) {
+                    Log.i("PREPROC", "Remaking grayhalf");
+                    grayHalf.create(mat.arrayHeight() / div, mat.arrayWidth() / div, CV_8UC1);
+                }
+                cvtColor(mat, gray, COLOR_RGB2GRAY);
+                cvResize(gray.asIplImage(), grayHalf.asIplImage());
+                cvSmooth(grayHalf.asIplImage(), grayHalf.asIplImage());
+
+                circles = cvHoughCircles(grayHalf.asIplImage(), str, opencv_imgproc.CV_HOUGH_GRADIENT, 1, grayHalf.rows() / 8, canny, ctr, 20, 400);
+            }
+
+            @Override
+            public synchronized void draw(Canvas canvas) {
+                if (circles == null) return;
+                Paint p = new Paint();
+                p.setStrokeWidth(4);
+                p.setStyle(Paint.Style.STROKE);
+                p.setColor(Color.GREEN);
+                Log.i("CIRCLE", new opencv_core.CvPoint3D32f(cvGetSeqElem(circles, 0)).toString());
+
+                for (int i = 0; (i < circles.total()) && (i < 20); i++) {
+                    opencv_core.CvPoint3D32f pt = new opencv_core.CvPoint3D32f(cvGetSeqElem(circles, i));
+                    canvas.drawCircle(pt.x() * div, pt.y() * div, pt.z() * div, p);
+                }
+            }
+        };
+        Mat img = opencv_highgui.imread(getRealPathFromURI(this, uri));
+        Mat mat = new Mat();
+        mat.create(1080,1920,CV_8UC3);
+        cvResize(img.asIplImage(), mat.asIplImage());
+        mc.handleMat(mat);
+        bmp = uriToBitmap(uri);
+        setContentView(layout);
+    }
+
+    class Preview extends View {
+        public Preview(Context context) {
+            super(context);
+
+        }
+
+        @Override
+        protected void onDraw(Canvas canvas) {
+            canvas.drawBitmap(bmp, new Rect(0,0,bmp.getWidth(),bmp.getHeight()), new Rect(0,0,1920,1080), new Paint());
+            mc.draw(canvas);
+        }
+
+
+    }
+
+    public String getRealPathFromURI(Context context, Uri contentUri) {
+        Cursor cursor = null;
+        try {
+            String[] proj = { MediaStore.Images.Media.DATA };
+            cursor = context.getContentResolver().query(contentUri,  proj, null, null, null);
+            int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+            cursor.moveToFirst();
+            return cursor.getString(column_index);
+        } finally {
+            if (cursor != null) {
+                cursor.close();
+            }
+        }
+    }
+
+
+    private Bitmap uriToBitmap(Uri selectedFileUri) {
+        Bitmap image;
+        try {
+            ParcelFileDescriptor parcelFileDescriptor =
+                    getContentResolver().openFileDescriptor(selectedFileUri, "r");
+            FileDescriptor fileDescriptor = parcelFileDescriptor.getFileDescriptor();
+            image = BitmapFactory.decodeFileDescriptor(fileDescriptor);
+
+
+            parcelFileDescriptor.close();
+            return image;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+
+
+
+
+}
